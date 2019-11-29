2f8f9dbfe0c15b24324a3a4813d4f9d4c4687ffd
Thomas Kittoe <tkittoe@lhrfvfyg0yrjk7m.fcc.pri>
2019-11-29 15:49:21 +0000

    Initialisation

---
+++ src/pages/Topics/Topics.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Topics.scss';
+
+const Topics = () => {
+    return(
+        <div id="topicsWrapper" className="wrapper">
+            <h1 id="topicsHeader" className="header">Topics</h1>
+        </div>
+    )
+}
+
+export default Topics;
