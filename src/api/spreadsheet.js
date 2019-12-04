52e1c1895743f187d884535b02e20d3292d9db03
TJ <t_kittoe@hotmail.co.uk>
2019-12-04 13:45:47 +0000

    updated key

--- src/api/spreadsheet.js
+++ src/api/spreadsheet.js
@@ -3,7 +3,7 @@ import Tabletop from 'tabletop';
 export const apiCall = () => {
     return new Promise((resolve, reject) => {
         Tabletop.init({
-            key: '1Bh5AV7LwiiWOlK6G-kVDX8YiWJNEyLrrYU6WEYnb_lg',
+            key: '1nogRbK3sIbF0T2bUPar9YLqvKPUWwFaSk5jk_hGBq-g',
             callback: googleData => {
                 console.log('google sheet data --->', googleData)
             },
