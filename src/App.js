a41c0d0b07c0398908d91ac403a74af45d70aa2e
isaacsquires <isaacsquires@me.com>
2019-12-04 16:54:14 +0000

    function for calling array of tag0

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Landing from './pages/Landing/Landing';
 import Topics from './pages/Topics/Topics';
-import { apiCall } from './api/spreadsheet';
+import { importSheet, getTag0, getTags, rows } from './api/spreadsheet';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   useEffect(() => {
-    apiCall();
+    var tag0Array = [];
+    // var allTagsArray = [];
+    // var tag0 = 'MIMs'
+    tag0Array = getTag0();
+    // allTagsArray = getTag0().then(() => {getTags(rows, tag0)});
+    console.log(tag0Array);
+    // console.log(allTagsArray);
   });
 
   return (
--- src/api/spreadsheet.js
+++ src/api/spreadsheet.js
@@ -1,13 +1,53 @@
 import Tabletop from 'tabletop';
 
-export const apiCall = () => {
+export var rows;
+
+export const getTag0 = () => {
     return new Promise((resolve, reject) => {
         Tabletop.init({
             key: '1nogRbK3sIbF0T2bUPar9YLqvKPUWwFaSk5jk_hGBq-g',
             callback: googleData => {
-                console.log('google sheet data --->', googleData)
+                // all data stored in data
+                var data = googleData['standards-datastore'];
+                // console.log('data --->', data);
+
+                // rows stored in rows
+                rows = data['elements'];
+                var tag0Array = []
+                for (let step = 0; step < Object.keys(rows).length; step++) {
+                    var tag0 = rows[step]['tag0'];
+                    tag0Array.push(tag0)
+                    }
+                var tag0Unique = [];
+                resolve(tag0Unique = Array.from(new Set(tag0Array)));
+                // console.log('tag0 --->', tag0Unique);
             },
-            simpleSheet: true
-        })
+        });
     });
 }
+
+
+// export const getTags = (rows, tag0) => {
+//     return new Promise((resolve, reject) => {
+//         var tagAll = []
+//         var filteredStandards = []
+//         for (let step = 0; step < Object.keys(rows).length; step++) {
+//             if (rows[step]['tag0'] === tag0) {
+//                 tagAll.push(rows[step]['tag1']);
+//                 tagAll.push(rows[step]['tag2']);
+//                 tagAll.push(rows[step]['tag3']);
+//                 tagAll.push(rows[step]['tag4']);
+//                 tagAll.push(rows[step]['tag5']);
+//                 tagAll.push(rows[step]['tag6']);
+//                 tagAll.push(rows[step]['tag7']);
+//                 filteredStandards.push(rows[step]);
+//             }
+//             }
+//         var tagAllUnique = [];
+//         resolve(tagAllUnique = Array.from(new Set(tagAll.filter(function (el) {
+//             return el !== "";
+//             }))));
+//         // console.log('All tags for tag0 --->',tag0, tagAllUnique);
+//         // console.log('All standards for these tag0 --->',tag0, filteredStandards);
+//     });
+// }
