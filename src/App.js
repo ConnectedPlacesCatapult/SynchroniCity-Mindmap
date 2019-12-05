9591626b40bdb9f7ccf8d9b90da89637a78a1810
Christina Last <clast@lhrfvfyg2gahv2h.fcc.pri>
2019-12-05 16:39:40 +0000

    attempt at Standards page

--- src/App.js
+++ src/App.js
@@ -2,45 +2,30 @@ import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Landing from './pages/Landing/Landing';
 import Topics from './pages/Topics/Topics';
-import Folder from './components/Folder/Folder';
-import SynchronicityLogo from './components/Synchronicity/SynchronicityLogo';
-import {
-  getTag0,
-  getTags,
-  rows,
-  tag0Unique,
-  tagAllUnique
-} from './api/spreadsheet';
-import generateContent from './functions/generateContent.jsx';
+import Standards from './pages/Standards/Standards';
+import { importSheet, getTag0, getTags, rows, tag0Unique, tagAllUnique, filteredStandards, filteredSubStandards, getFilteredSubStandards } from './api/spreadsheet';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-const folderRed = require('./images/folder_red.png')
-
 function App() {
   useEffect(() => {
-    // tag0 = click button
-    var tag0 = 'MIMs';
-    getTag0().then(() => { getTags(rows, tag0) })
-      .then(() => { generateContent.tag0(); })
-      .then(() => {
-        for (let i = 1; i <= tag0Unique.length; i++) {
-          document.querySelector('.topicDiv:nth-child(' + i + ')').innerHTML = <Folder /> + document.querySelector('.topicDiv:nth-child(' + i + ')').innerHTML
-          let test = document.querySelector('.topicDiv:nth-child(12)');
-          console.log(test)
-        }
-      });
-
-    console.log(rows);
-
-  }); 
+    // tag0 = click button topic
+    var tag0Select = 'MIMs'
+    // subtag = click button subtopic
+    var subTagSelect = 'API'
+    getTag0().then(() => {getTags(rows, tag0Select)}).then(() => {getFilteredSubStandards(filteredStandards, subTagSelect)}).then(() => console.log(filteredSubStandards));
+    // tag0Unique stores all topics
+    // tagAllUnique stores all subtopics based on a topic
+    // filteredStandards stores all standards based on a topic
+    // filteredSubStandard stores all standards based on a subtopic and topic
+  });
 
   return (
     <div className="App">
       <Navbar />
       {/* <Landing /> */}
       <Topics />
-      <Folder />
+      <Standards />
     </div>
   );
 }
---
+++ src/pages/Standards/Standards.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Standards.css';
+
+const Standards = () => {
+    <div id= "standardsContainer">
+        <div className = "standardsResult row">
+            <div className = "standardsNavigationContainer col-lg=3 col-md-3 offset-lg-1 offset-md-1">
+            </div>
+            <div className = "standardsContentContainer col-lg=6 col-md-6 col-sm-10 offset-lg-1 offset-md-1 offset-sm-1">
+                <h3>Standard Name</h3>
+                <h4>Subtitle</h4>
+                <p>Description</p>
+                <a href="#">Link to Standard</a>
+            </div>
+        </div>
+    </div>
+
+}
+
+export default Standards;
