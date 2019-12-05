0c632e601da770299858273788a71c3ceb80362c
isaacsquires <isaacsquires@me.com>
2019-12-05 14:58:46 +0000

    subtopic standard filtering

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Landing from './pages/Landing/Landing';
 import Topics from './pages/Topics/Topics';
-import { importSheet, getTag0, getTags, rows, tag0Unique, tagAllUnique } from './api/spreadsheet';
+import { importSheet, getTag0, getTags, rows, tag0Unique, tagAllUnique, filteredStandards, filteredSubStandards, getFilteredSubStandards } from './api/spreadsheet';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   useEffect(() => {
-    // tag0 = click button
+    // tag0 = click button topic
     var tag0 = 'MIMs'
-    getTag0().then(() => {getTags(rows, tag0)}).then(() => console.log(tagAllUnique));
-    
+    // subtag = click button subtopic
+    var subtag = 'API'
+    getTag0().then(() => {getTags(rows, tag0)}).then(() => {getFilteredSubStandards(filteredStandards, subtag)}).then(() => console.log(filteredSubStandards));
+    // tag0Unique stores all topics
+    // tagAllUnique stores all subtopics based on a topic
+    // filteredStandards stores all standards based on a topic
+    // filteredSubStandard stores all standards based on a subtopic and topic
   });
 
   return (
