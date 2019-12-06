1072b6292db1ecab5ac269fb4af0fc2fc90b98f8
isaacsquires <isaacsquires@me.com>
2019-12-06 10:58:42 +0000

    github pages deployment

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Landing from './pages/Landing/Landing';
 import Topics from './pages/Topics/Topics';
-<<<<<<< HEAD
-import { importSheet, getTag0, getTags, rows, tag0Unique, tagAllUnique, filteredStandards, filteredSubStandards, getFilteredSubStandards } from './api/spreadsheet';
-=======
 import Folder from './components/Folder/Folder';
 import SynchronicityLogo from './components/Synchronicity/SynchronicityLogo';
+// import { importSheet, getTag0, getTags, rows, tag0Unique, tagAllUnique, filteredStandards, filteredSubStandards, getFilteredSubStandards } from './api/spreadsheet';
 import {
   getTag0,
   getTags,
@@ -15,7 +13,6 @@ import {
   tagAllUnique
 } from './api/spreadsheet';
 import generateContent from './functions/generateContent.jsx';
->>>>>>> f19dbde8347e5f253e6fcf2fdc941e61a5ecc2da
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
@@ -23,18 +20,6 @@ const folderRed = require('./images/folder_red.png')
 
 function App() {
   useEffect(() => {
-<<<<<<< HEAD
-    // tag0 = click button topic
-    var tag0 = 'MIMs'
-    // subtag = click button subtopic
-    var subtag = 'API'
-    getTag0().then(() => {getTags(rows, tag0)}).then(() => {getFilteredSubStandards(filteredStandards, subtag)}).then(() => console.log(filteredSubStandards));
-    // tag0Unique stores all topics
-    // tagAllUnique stores all subtopics based on a topic
-    // filteredStandards stores all standards based on a topic
-    // filteredSubStandard stores all standards based on a subtopic and topic
-  });
-=======
     // tag0 = click button
     var tag0 = 'MIMs';
     getTag0().then(() => { getTags(rows, tag0) })
@@ -50,8 +35,19 @@ function App() {
     console.log(rows);
 
   }); 
->>>>>>> f19dbde8347e5f253e6fcf2fdc941e61a5ecc2da
 
+
+      // // tag0 = click button topic
+    // var tag0 = 'MIMs'
+    // // subtag = click button subtopic
+    // var subtag = 'API'
+    // getTag0().then(() => {getTags(rows, tag0)}).then(() => {getFilteredSubStandards(filteredStandards, subtag)}).then(() => console.log(filteredSubStandards));
+    // // tag0Unique stores all topics
+    // // tagAllUnique stores all subtopics based on a topic
+    // // filteredStandards stores all standards based on a topic
+    // // filteredSubStandard stores all standards based on a subtopic and topic
+
+    
   return (
     <div className="App">
       <Navbar />
