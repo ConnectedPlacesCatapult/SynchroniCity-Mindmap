231e0a7bbcbf2032f91fd6c0692382167d475037
isaacsquires <isaacsquires@me.com>
2019-12-06 10:34:25 +0000

    Updates

---
+++ precache-manifest.43777dadf2201f1df6a645000b7d920e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728a8a73089f1a922fe4f4c03f6dee4b",
+    "url": "/SynchroniCity-Mindmap/index.html"
+  },
+  {
+    "revision": "93a5a14a541c3687f116",
+    "url": "/SynchroniCity-Mindmap/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "79d3d282d3c6a3cbde32",
+    "url": "/SynchroniCity-Mindmap/static/css/main.83f6b316.chunk.css"
+  },
+  {
+    "revision": "93a5a14a541c3687f116",
+    "url": "/SynchroniCity-Mindmap/static/js/2.4c1d49a1.chunk.js"
+  },
+  {
+    "revision": "79d3d282d3c6a3cbde32",
+    "url": "/SynchroniCity-Mindmap/static/js/main.469e9ccb.chunk.js"
+  },
+  {
+    "revision": "ca981e2185e3144cc6a0",
+    "url": "/SynchroniCity-Mindmap/static/js/runtime-main.aed966f6.js"
+  },
+  {
+    "revision": "08c287f151a219980911637df30f79f9",
+    "url": "/SynchroniCity-Mindmap/static/media/bg.08c287f1.jpg"
+  }
+]);
