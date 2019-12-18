a8cccb4416ff9c8771764c554a7e2ee294f6e048
TJ <t_kittoe@hotmail.co.uk>
2019-12-18 13:07:57 +0000

    latest

--- src/pages/Standards/Standards.jsx
+++ src/pages/Standards/Standards.jsx
@@ -8,13 +8,16 @@ const Standards = () => {
         <div id="resultWrapper">
             <div id="standardsResult" className="standardsResult row">
                 <div id="backdrop" onClick={() => { generateContent.closeModal(); }}></div>
-                <div className="standardsContentContainer col-lg=6 col-md-6 col-sm-10 offset-lg-3 offset-md-3 offset-sm-1">
+                <div className="standardsContentContainer col-lg-6 col-md-6 col-sm-10 offset-lg-3 offset-md-3 offset-sm-1">
+                    <img id="resultTop" src={require('../../images/Close_Container.svg')} />
                     <div id="closeStandard" onClick={() => { generateContent.closeModal(); }}></div>
-                    <h3 id="standardName"> Standard</h3>
-                    <h4 id="subTitle"> Subtitle</h4>
+                    <div id="titleAndSubtitle">
+                        <h3 id="standardName"> Standard</h3>
+                        <h3 id="subTitle"> Subtitle</h3>
+                    </div>
                     <p id="description"> Description</p>
                     <div id="URLDiv">
-                        <a id="URL" href="#" target="_blank"> Link to Standard</a>
+                        <a id="URL" href="#" target="_blank"> link to standard</a>
                     </div>
 
                 </div>
