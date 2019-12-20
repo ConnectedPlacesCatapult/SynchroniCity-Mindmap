c0ce91f7e46904dc4af90a4303920f4830782a0c
TJ <t_kittoe@hotmail.co.uk>
2019-12-20 15:38:30 +0000

    Updates

---
+++ precache-manifest.6d4ec5c3c40f75f07813037f4a9961bf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e2bfaacc2a141789f6624373ba3db0",
+    "url": "/SynchroniCity-Mindmap/index.html"
+  },
+  {
+    "revision": "5f4d4f9cea7652a92741",
+    "url": "/SynchroniCity-Mindmap/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "da0b13bef82ecd59fadb",
+    "url": "/SynchroniCity-Mindmap/static/css/main.8a2ecb4a.chunk.css"
+  },
+  {
+    "revision": "5f4d4f9cea7652a92741",
+    "url": "/SynchroniCity-Mindmap/static/js/2.95505e67.chunk.js"
+  },
+  {
+    "revision": "da0b13bef82ecd59fadb",
+    "url": "/SynchroniCity-Mindmap/static/js/main.69d3e9d2.chunk.js"
+  },
+  {
+    "revision": "ca981e2185e3144cc6a0",
+    "url": "/SynchroniCity-Mindmap/static/js/runtime-main.aed966f6.js"
+  },
+  {
+    "revision": "717065fb7648fa8cb5016e66c2cfaa94",
+    "url": "/SynchroniCity-Mindmap/static/media/Card_Result_Active.717065fb.svg"
+  },
+  {
+    "revision": "f3d57f8393a97d31d451d92fe9cb82c7",
+    "url": "/SynchroniCity-Mindmap/static/media/Close_Container.f3d57f83.svg"
+  },
+  {
+    "revision": "070c78ea098fc14a22ab44adb6e954b8",
+    "url": "/SynchroniCity-Mindmap/static/media/Description_Box.070c78ea.svg"
+  },
+  {
+    "revision": "5bbc337d9a7745f4bff31e153175b6f5",
+    "url": "/SynchroniCity-Mindmap/static/media/Description_Box_Bottom.5bbc337d.svg"
+  },
+  {
+    "revision": "44bf906f7ed7a43984528ed754806b49",
+    "url": "/SynchroniCity-Mindmap/static/media/Top_Box_About.44bf906f.svg"
+  },
+  {
+    "revision": "3aa8da4c07c4d1f689c27c3eecc85598",
+    "url": "/SynchroniCity-Mindmap/static/media/Top_Box_GetInvolved.3aa8da4c.svg"
+  },
+  {
+    "revision": "7c201e7b31f2ef576f31d48b33bf5061",
+    "url": "/SynchroniCity-Mindmap/static/media/bg.7c201e7b.jpg"
+  },
+  {
+    "revision": "e52c1fb11ee17f440f748f7861d49a97",
+    "url": "/SynchroniCity-Mindmap/static/media/bkgn_generic.e52c1fb1.jpg"
+  },
+  {
+    "revision": "e87f9bbac640bfb0a10a3af92165269b",
+    "url": "/SynchroniCity-Mindmap/static/media/bkgn_generic_top.e87f9bba.png"
+  },
+  {
+    "revision": "445f2174d9b978e9cb0e91bf192bb937",
+    "url": "/SynchroniCity-Mindmap/static/media/search.445f2174.png"
+  },
+  {
+    "revision": "7beeff5c1082d5ec13fda087e967e1a5",
+    "url": "/SynchroniCity-Mindmap/static/media/synchronicity_logo_black.7beeff5c.svg"
+  }
+]);
